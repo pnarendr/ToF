f0e7ddd0e9980b41b091037db7f2e6709409974e
Pat Narendra <pat.narendra@gmail.com>
2021-09-15 17:46:27 -0500

    first commit.  converted to Kotlin

--- app/src/main/java/com/example/tof/Camera.kt
+++ app/src/main/java/com/example/tof/Camera.kt
@@ -1,149 +1,162 @@
-package com.example.tof;
+package com.example.tof
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.graphics.ImageFormat;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraCaptureSession;
-import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraDevice;
-import android.hardware.camera2.CameraManager;
-import android.hardware.camera2.CameraMetadata;
-import android.hardware.camera2.CaptureRequest;
-import android.media.ImageReader;
-import android.util.Log;
-import android.util.Range;
-import android.util.SizeF;
-import android.view.Surface;
+import android.Manifest
+import android.content.Context
+import com.example.tof.DepthFrameVisualizer
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
+import android.hardware.camera2.CaptureRequest
+import com.example.tof.DepthFrameAvailableListener
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraMetadata
+import android.util.SizeF
+import android.hardware.camera2.CameraAccessException
+import androidx.core.content.ContextCompat
+import android.content.pm.PackageManager
+import android.hardware.camera2.CameraCaptureSession
+import android.graphics.ImageFormat
+import android.media.ImageReader
+import android.util.Log
+import android.util.Range
+import java.lang.IllegalStateException
+import java.util.*
 
-import java.util.Arrays;
-import java.util.List;
+class Camera(private val context: Context, depthFrameVisualizer: DepthFrameVisualizer?) :
+    CameraDevice.StateCallback() {
+    private val cameraManager: CameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+    private val previewReader: ImageReader = ImageReader.newInstance(
+        DepthFrameAvailableListener.WIDTH,
+        DepthFrameAvailableListener.HEIGHT, ImageFormat.DEPTH16, 2
+    )
+    private var previewBuilder: CaptureRequest.Builder? = null
+    private val imageAvailableListener: DepthFrameAvailableListener = DepthFrameAvailableListener(depthFrameVisualizer)
 
-import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
+    fun listCameras(){
+        cameraManager.cameraIdList.forEach {camera ->
+            val char = cameraManager.getCameraCharacteristics(camera)
 
-public class Camera extends CameraDevice.StateCallback {
-    private static final String TAG = Camera.class.getSimpleName();
-
-    private static int FPS_MIN = 15;
-    private static int FPS_MAX = 30;
-
-    private Context context;
-    private CameraManager cameraManager;
-    private ImageReader previewReader;
-    private CaptureRequest.Builder previewBuilder;
-    private DepthFrameAvailableListener imageAvailableListener;
-
-    public Camera(Context context, DepthFrameVisualizer depthFrameVisualizer) {
-        this.context = context;
-        cameraManager = (CameraManager)context.getSystemService(Context.CAMERA_SERVICE);
-        imageAvailableListener = new DepthFrameAvailableListener(depthFrameVisualizer);
-        previewReader = ImageReader.newInstance(DepthFrameAvailableListener.WIDTH,
-                DepthFrameAvailableListener.HEIGHT, ImageFormat.DEPTH16,2);
-        previewReader.setOnImageAvailableListener(imageAvailableListener, null);
+            Log.d(TAG, "cameras ${char.get(CameraCharacteristics.REQUEST_AVAILABLE_CAPABILITIES)}")
+        }
     }
 
     // Open the front depth camera and start sending frames
-    public void openFrontDepthCamera() {
-        final String cameraId = getFrontDepthCameraID();
-        openCamera(cameraId);
+    fun openFrontDepthCamera() {
+        val cameraId = frontDepthCameraID
+        openCamera(cameraId)
     }
 
-    private String getFrontDepthCameraID() {
-        try {
-            for (String camera : cameraManager.getCameraIdList()) {
-                CameraCharacteristics chars = cameraManager.getCameraCharacteristics(camera);
-                final int[] capabilities = chars.get(CameraCharacteristics.REQUEST_AVAILABLE_CAPABILITIES);
-                boolean facingFront = chars.get(CameraCharacteristics.LENS_FACING) == CameraMetadata.LENS_FACING_FRONT;
-                boolean depthCapable = false;
-                for (int capability : capabilities) {
-                    boolean capable = capability == CameraMetadata.REQUEST_AVAILABLE_CAPABILITIES_DEPTH_OUTPUT;
-                    depthCapable = depthCapable || capable;
-                }
-                if (depthCapable && facingFront) {
-                    // Note that the sensor size is much larger than the available capture size
-                    SizeF sensorSize = chars.get(CameraCharacteristics.SENSOR_INFO_PHYSICAL_SIZE);
-                    Log.i(TAG, "Sensor size: " + sensorSize);
+    // Note that the sensor size is much larger than the available capture size
+    private val frontDepthCameraID:
 
-                    // Since sensor size doesn't actually match capture size and because it is
-                    // reporting an extremely wide aspect ratio, this FoV is bogus
-                    float[] focalLengths = chars.get(CameraCharacteristics.LENS_INFO_AVAILABLE_FOCAL_LENGTHS);
-                    if (focalLengths.length > 0) {
-                        float focalLength = focalLengths[0];
-                        double fov = 2 * Math.atan(sensorSize.getWidth() / (2 * focalLength));
-                        Log.i(TAG, "Calculated FoV: " + fov);
+    // Since sensor size doesn't actually match capture size and because it is
+    // reporting an extremely wide aspect ratio, this FoV is bogus
+            String?
+        get() {
+            try {
+                for (camera in cameraManager.cameraIdList) {
+                    val chars = cameraManager.getCameraCharacteristics(camera)
+                    val capabilities =
+                        chars.get(CameraCharacteristics.REQUEST_AVAILABLE_CAPABILITIES)
+                    val facingFront =
+                        chars.get(CameraCharacteristics.LENS_FACING) == CameraMetadata.LENS_FACING_FRONT
+                    var depthCapable = false
+                    for (capability in capabilities!!) {
+                        val capable =
+                            capability == CameraMetadata.REQUEST_AVAILABLE_CAPABILITIES_DEPTH_OUTPUT
+                        depthCapable = depthCapable || capable
                     }
-                    return camera;
+                    if (depthCapable && facingFront) {
+                        // Note that the sensor size is much larger than the available capture size
+                        val sensorSize = chars.get(CameraCharacteristics.SENSOR_INFO_PHYSICAL_SIZE)
+                        Log.i(TAG, "Sensor size: $sensorSize")
+
+                        // Since sensor size doesn't actually match capture size and because it is
+                        // reporting an extremely wide aspect ratio, this FoV is bogus
+                        val focalLengths =
+                            chars.get(CameraCharacteristics.LENS_INFO_AVAILABLE_FOCAL_LENGTHS)
+                        if (focalLengths?.isNotEmpty() == true) {
+                            val focalLength = focalLengths[0]
+                            val fov =
+                                2 * Math.atan((sensorSize!!.width / (2 * focalLength)).toDouble())
+                            Log.i(TAG, "Calculated FoV: $fov")
+                        }
+                        return camera
+                    }
                 }
+            } catch (e: CameraAccessException) {
+                Log.e(TAG, "Could not initialize Camera Cache")
+                e.printStackTrace()
             }
-        } catch (CameraAccessException e) {
-            Log.e(TAG, "Could not initialize Camera Cache");
-            e.printStackTrace();
+            return null
         }
-        return null;
+
+    private fun openCamera(cameraId: String?) {
+        if (cameraId != null) {
+            try {
+                val permission = ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+                if (PackageManager.PERMISSION_GRANTED == permission) {
+                    cameraManager.openCamera(cameraId, this, null)
+                } else {
+                    Log.e(TAG, "Permission not available to open camera")
+                }
+            } catch (e: CameraAccessException) {
+                Log.e(TAG, "Opening Camera has an Exception $e")
+                e.printStackTrace()
+            } catch (e: IllegalStateException) {
+                Log.e(TAG, "Opening Camera has an Exception $e")
+                e.printStackTrace()
+            } catch (e: SecurityException) {
+                Log.e(TAG, "Opening Camera has an Exception $e")
+                e.printStackTrace()
+            }
+        }
+
     }
 
-    private void openCamera(String cameraId) {
-        try{
-            int permission = ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA);
-            if(PackageManager.PERMISSION_GRANTED == permission) {
-                cameraManager.openCamera(cameraId, this, null);
-            }else{
-                Log.e(TAG,"Permission not available to open camera");
-            }
-        }catch (CameraAccessException | IllegalStateException | SecurityException e){
-            Log.e(TAG,"Opening Camera has an Exception " + e);
-            e.printStackTrace();
-        }
-    }
-
-
-    @Override
-    public void onOpened(@NonNull CameraDevice camera) {
+    override fun onOpened(camera: CameraDevice) {
         try {
-            previewBuilder = camera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-            previewBuilder.set(CaptureRequest.JPEG_ORIENTATION, 0);
-            Range<Integer> fpsRange = new Range<>(FPS_MIN, FPS_MAX);
-            previewBuilder.set(CaptureRequest.CONTROL_AE_TARGET_FPS_RANGE, fpsRange);
-            previewBuilder.addTarget(previewReader.getSurface());
-
-            List<Surface> targetSurfaces = Arrays.asList(previewReader.getSurface());
+            previewBuilder = camera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+            previewBuilder?.set(CaptureRequest.JPEG_ORIENTATION, 0)
+            val fpsRange = Range(FPS_MIN, FPS_MAX)
+            previewBuilder?.set(CaptureRequest.CONTROL_AE_TARGET_FPS_RANGE, fpsRange)
+            previewBuilder?.addTarget(previewReader.surface)
+            val targetSurfaces = Arrays.asList(previewReader.surface)
             camera.createCaptureSession(targetSurfaces,
-                    new CameraCaptureSession.StateCallback() {
-                        @Override
-                        public void onConfigured(@NonNull CameraCaptureSession session) {
-                            onCaptureSessionConfigured(session);
-                        }
-                        @Override
-                        public void onConfigureFailed(@NonNull CameraCaptureSession session) {
-                            Log.e(TAG,"!!! Creating Capture Session failed due to internal error ");
-                        }
-                    }, null);
+                object : CameraCaptureSession.StateCallback() {
+                    override fun onConfigured(session: CameraCaptureSession) {
+                        onCaptureSessionConfigured(session)
+                    }
 
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
+                    override fun onConfigureFailed(session: CameraCaptureSession) {
+                        Log.e(TAG, "!!! Creating Capture Session failed due to internal error ")
+                    }
+                }, null
+            )
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
         }
     }
 
-    private void onCaptureSessionConfigured(@NonNull CameraCaptureSession session) {
-        Log.i(TAG,"Capture Session created");
-        previewBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+    private fun onCaptureSessionConfigured(session: CameraCaptureSession) {
+        Log.i(TAG, "Capture Session created")
+        previewBuilder?.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO)
         try {
-            session.setRepeatingRequest(previewBuilder.build(), null, null);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
+            session.setRepeatingRequest(previewBuilder!!.build(), null, null)
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
         }
     }
 
+    override fun onDisconnected(camera: CameraDevice) {}
+    override fun onError(camera: CameraDevice, error: Int) {}
 
-    @Override
-    public void onDisconnected(@NonNull CameraDevice camera) {
-
+    companion object {
+        private val TAG = Camera::class.java.simpleName
+        private const val FPS_MIN = 15
+        private const val FPS_MAX = 30
     }
 
-    @Override
-    public void onError(@NonNull CameraDevice camera, int error) {
-
+    init {
+        previewReader.setOnImageAvailableListener(imageAvailableListener, null)
     }
 }
--- app/src/main/java/com/example/tof/MainActivity.kt
+++ app/src/main/java/com/example/tof/MainActivity.kt
@@ -1,18 +1,16 @@
-package com.example.tof;
+package com.example.tof
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Matrix;
-import android.graphics.RectF;
-import android.os.Bundle;
-import android.view.TextureView;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.graphics.RectF
+import android.os.Bundle
+import android.view.TextureView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.tof.MainActivity
 
 /*  This is an example of getting and processing ToF data.
 
@@ -20,60 +18,64 @@ import androidx.core.content.ContextCompat;
     with output in DEPTH16. The constants can be adjusted but are made assuming this
     is being run on a Samsung S10 5G device.
  */
-public class MainActivity extends AppCompatActivity implements DepthFrameVisualizer {
-    private static final String TAG = MainActivity.class.getSimpleName();
-    public static final int CAM_PERMISSIONS_REQUEST = 0;
+class MainActivity : AppCompatActivity(), DepthFrameVisualizer {
+    private var rawDataView: TextureView? = null
+    private var noiseReductionView: TextureView? = null
+    private var movingAverageView: TextureView? = null
+    private var blurredAverageView: TextureView? = null
+    private var defaultBitmapTransform: Matrix? = null
+    private var camera: Camera? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        rawDataView = findViewById(R.id.rawData)
+        noiseReductionView = findViewById(R.id.noiseReduction)
+        movingAverageView = findViewById(R.id.movingAverage)
+        blurredAverageView = findViewById(R.id.blurredAverage)
+        checkCamPermissions()
+        camera = Camera(this, this)
+        camera?.listCameras()
 
-    private TextureView rawDataView;
-    private TextureView noiseReductionView;
-    private TextureView movingAverageView;
-    private TextureView blurredAverageView;
-    private Matrix defaultBitmapTransform;
-    private Camera camera;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        rawDataView = findViewById(R.id.rawData);
-        noiseReductionView = findViewById(R.id.noiseReduction);
-        movingAverageView = findViewById(R.id.movingAverage);
-        blurredAverageView = findViewById(R.id.blurredAverage);
-
-        checkCamPermissions();
-        camera = new Camera(this, this);
-        camera.openFrontDepthCamera();
+        //todo revert later
+        // camera?.openFrontDepthCamera()
     }
 
-    private void checkCamPermissions() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAM_PERMISSIONS_REQUEST);
+    private fun checkCamPermissions() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                CAM_PERMISSIONS_REQUEST
+            )
         }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
-    @Override
-    public void onRawDataAvailable(Bitmap bitmap) {
-        renderBitmapToTextureView(bitmap, rawDataView);
+    override fun onRawDataAvailable(bitmap: Bitmap) {
+        renderBitmapToTextureView(bitmap, rawDataView)
     }
 
-    @Override
-    public void onNoiseReductionAvailable(Bitmap bitmap) {
-        renderBitmapToTextureView(bitmap, noiseReductionView);
+    override fun onNoiseReductionAvailable(bitmap: Bitmap) {
+        renderBitmapToTextureView(bitmap, noiseReductionView)
     }
 
-    @Override
-    public void onMovingAverageAvailable(Bitmap bitmap) {
-        renderBitmapToTextureView(bitmap, movingAverageView);
+    override fun onMovingAverageAvailable(bitmap: Bitmap) {
+        renderBitmapToTextureView(bitmap, movingAverageView)
     }
 
-    @Override
-    public void onBlurredMovingAverageAvailable(Bitmap bitmap) {
-        renderBitmapToTextureView(bitmap, blurredAverageView);
+    override fun onBlurredMovingAverageAvailable(bitmap: Bitmap) {
+        renderBitmapToTextureView(bitmap, blurredAverageView)
     }
 
     /* We don't want a direct camera preview since we want to get the frames of data directly
@@ -82,34 +84,33 @@ public class MainActivity extends AppCompatActivity implements DepthFrameVisuali
         This takes a converted bitmap and renders it onto the surface, with a basic rotation
         applied.
      */
-    private void renderBitmapToTextureView(Bitmap bitmap, TextureView textureView) {
-        Canvas canvas = textureView.lockCanvas();
-        canvas.drawBitmap(bitmap, defaultBitmapTransform(textureView), null);
-        textureView.unlockCanvasAndPost(canvas);
+    private fun renderBitmapToTextureView(bitmap: Bitmap, textureView: TextureView?) {
+        val canvas = textureView!!.lockCanvas()
+        canvas!!.drawBitmap(bitmap, defaultBitmapTransform(textureView)!!, null)
+        textureView.unlockCanvasAndPost(canvas)
     }
 
-    private Matrix defaultBitmapTransform(TextureView view) {
-        if (defaultBitmapTransform == null || view.getWidth() == 0 || view.getHeight() == 0) {
-            Matrix matrix = new Matrix();
-            int centerX = view.getWidth() / 2;
-            int centerY = view.getHeight() / 2;
-
-            int bufferWidth = DepthFrameAvailableListener.WIDTH;
-            int bufferHeight = DepthFrameAvailableListener.HEIGHT;
-
-            RectF bufferRect = new RectF(0, 0, bufferWidth, bufferHeight);
-            RectF viewRect = new RectF(0, 0, view.getWidth(), view.getHeight());
-            matrix.setRectToRect(bufferRect, viewRect, Matrix.ScaleToFit.CENTER);
-            matrix.postRotate(270, centerX, centerY);
-
-            defaultBitmapTransform = matrix;
+    private fun defaultBitmapTransform(view: TextureView?): Matrix? {
+        if (defaultBitmapTransform == null || view!!.width == 0 || view.height == 0) {
+            val matrix = Matrix()
+            val centerX = view!!.width / 2
+            val centerY = view.height / 2
+            val bufferWidth = DepthFrameAvailableListener.WIDTH
+            val bufferHeight = DepthFrameAvailableListener.HEIGHT
+            val bufferRect = RectF(0f, 0f, bufferWidth.toFloat(), bufferHeight.toFloat())
+            val viewRect = RectF(
+                0f, 0f, view.width.toFloat(), view.height
+                    .toFloat()
+            )
+            matrix.setRectToRect(bufferRect, viewRect, Matrix.ScaleToFit.CENTER)
+            matrix.postRotate(270f, centerX.toFloat(), centerY.toFloat())
+            defaultBitmapTransform = matrix
         }
-        return defaultBitmapTransform;
+        return defaultBitmapTransform
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+        const val CAM_PERMISSIONS_REQUEST = 0
     }
 }
-
-
-
-
-
-
